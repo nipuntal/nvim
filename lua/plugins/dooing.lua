07d963166fb6d894de012a2faccfdcc25c9c3e01
Nipun Talagadivi <nipuntal11@gmail.com>
2026-03-12 15:22:04 +1100

    brev

--- lua/plugins/dooing.lua
+++ lua/plugins/dooing.lua
@@ -2,6 +2,9 @@ return {
   {
     "atiladefreitas/dooing",
     config = function()
+      local dooing_path = vim.fn.stdpath("config") .. "/data/dooing_todos.json"
+      vim.fn.mkdir(vim.fn.fnamemodify(dooing_path, ":h"), "p")
+
       require("dooing").setup({
         window = {
           width = 55,
@@ -9,7 +12,7 @@ return {
           border = "rounded",
         },
         quick_keys = false,
-        save_path = vim.fn.stdpath("config") .. "/data/dooing_todos.json",
+        save_path = dooing_path,
 
         formatting = {
           pending = {
