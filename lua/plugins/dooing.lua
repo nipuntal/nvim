0627632d03d9eabf90cd5d2a0f5501670fa9574e
Nipun Talagadivi <nipuntal11@gmail.com>
2026-03-12 14:47:49 +1100

    Final nvim push before automation

--- lua/plugins/dooing.lua
+++ lua/plugins/dooing.lua
@@ -10,6 +10,21 @@ return {
         },
         quick_keys = false,
         save_path = vim.fn.stdpath("config") .. "/data/dooing_todos.json",
+
+        formatting = {
+          pending = {
+            icon = "☐",
+            format = { "icon", "notes_icon", "text", "due_date", "ect" },
+          },
+          in_progress = {
+            icon = "▣",
+            format = { "icon", "text", "due_date", "ect" },
+          },
+          done = {
+            icon = "☒",
+            format = { "icon", "notes_icon", "text", "due_date", "ect" },
+          },
+        },
       })
     end,
   },
